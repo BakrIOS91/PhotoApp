66e287baa9a6d1ddf19373fe2c4d81427a850d82
Bakr Mohamed <Engbakrmohamed@hotmail.com>
2023-04-07 00:54:40 +0200

    Initial Commit

---
+++ PhotoApp/PhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAppApp.swift
+//  PhotoApp
+//
+//  Created by Bakr mohamed on 07/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
